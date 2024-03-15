f64c593fd3bc0ef622fc32b9826b2cb73ddee254
HARSH-TRIPATHI-59 <mynameisharsh143@gmail.com>
2024-03-15 21:27:07 +0530

    Day4Props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,15 @@
 import Button from "./button";
-import Card from "./card"
+import Card from "./card";
+import Student from "./Student/Student";
 function App() {
-  
-     return(
-      <>
-     <Button/>
-      </>
-        
-     );
-  
+  return (
+    <>
+      {/* <Button/> */}
+      <Student name="Harsh" age={19} isStudent={true} />
+      <Student name="Sahil" age={21} isStudent={true} />
+      <Student />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ my-react-app/src/Student/Student.jsx
@@ -0,0 +1,22 @@
+import styles from './student.module.css';
+import PropTypes from 'prop-types';
+function Student(props){
+    return(
+        <div className={styles.student}>
+            <p>Name: {props.name}</p>
+            <p>Age: {props.age}</p>
+            <p>Student: {props.isStudent?"Yes": "NO"}</p>
+        </div>
+    );
+}
+Student.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.bool,
+}
+Student.defaultProps = {
+    name: "Guest",
+    age: 19,
+    isStudent: false,
+}
+export default Student
